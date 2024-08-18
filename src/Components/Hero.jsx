928069f8b6b24d98c5791dbf235e6a7a56abe7cb
SaadRamzan <saadramzan1605@gmail.com>
2024-08-18 20:22:09 +0500

    2nd

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
                 <h1 className='hero-heading' ><p>Hi I'm "Your Name",</p> front end developer based in "Your Country".</h1>
                 <p className='hero-paragraph' >I am a frontend developer from "Your City", "Your Country" with 1 year of experience in Web-Development.I have served in multiple companies.</p>
                 <div className='hero-connect' >
-                    <span className='hero-connect-one'  >Connect with me</span>
+                    <span className='hero-connect-one'  >Connnect with me</span>
                     <span className='hero-connect-two' >My resume</span>
                 </div>
             </div>
